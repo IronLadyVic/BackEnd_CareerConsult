87a8b79b9f7434526b31cc42391f57f311c98c1d
unknown <victoria.clark@44385-WIN-501.auckland.natcoll.ac.nz>
2014-11-22 11:16:22 +1300

    4th commit

--- app/views/login.blade.php
+++ app/views/login.blade.php
@@ -8,23 +8,29 @@
     <p id="mobdeskDescription">Log in to update your career profile, or enquire about booking a service.</p>
 
     {{Form::open(array('url'=>'login', 'id'=>'mobdeskForm'))}}
-        <p><img id="profileImage" src="{{URL::to('careerprofile/'.$user->avatar)}}" alt="profile picture"></p>
+        <p><img id="profileImage" src="{{URL::to('img//careerprofile-BLK.png')}}" alt="profile picture"></p>
         
         <p>
         <span class="border"><i class="fa fa-user"></i></span>
-        {{Form::text('username', array('placeholder'=>'Username', 'class'=>'border'))}}
+        {{Form::text('username', '', array('placeholder'=>'Username', 'id'=>'userName', 'class'=>'border'))}}
+        {{$errors->first('username','<p class="border">:message</p>')}}
         </p>
 
         <p>
         <span class="border" ><i class="fa fa-lock"></i></span>       
-        {{Form::password('password', array('placeholder'=>'Password', 'class'=>'border'))}}     
+        {{Form::password('password', array('placeholder' => 'Password', 'id'=>'passwordLogin'))}}
+        {{$errors->first('password','<p class="border">:message</p>')}}     
         </p>
 
-        {{Form::checkbox(array('id'=>'checkBoxRemember', 'class'=>'border'))}} 
-        <p id="rememberMe">Remember Me</p>    
-
-    {{Form::submit('LOG IN', array('id'=>'loggingInButton', 'class'=>'border'))}}
         
+        <input type="checkbox" value="None" id="checkBoxRemember" name="check" />
+        <label for="checkBoxRemember"></label>
+        <p  id="rememberMe">Remember Me</p>
+        
+        
+        <label for="loggingInButton"></label>
+        <button type="submit" name="button" id="loggingInButton" class="button">LOG IN</button>
+
 {{Form::close()}}
 {{Session::get("error")}}
 
